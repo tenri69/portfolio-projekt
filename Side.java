f6ca68f325b09b9945daaab522e1b167a42157f9
tenri69 <henkawesterholm@gmail.com>
2025-11-24 21:42:27 +0100

    Update Side.java

--- Side.java
+++ Side.java
@@ -8,4 +8,28 @@ public class Side {
         this.p1 = p1;
         this.p2 = p2;
     }
+
+    public Point getP1() {
+        return p1;
+
+    }
+    public Point getP2() {
+        return p2;
+
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Side other = (Side) obj;
+
+        return (this.getP1().equals(other.getP1()) && this.getP2().equals(other.getP2())) ||
+                (this.getP1().equals(other.getP2()) && this.getP2().equals(other.getP1()));
+    }
+
+    @Override
+    public int hashCode() {
+        return p1.hashCode() + p2.hashCode();
+    }
 }
